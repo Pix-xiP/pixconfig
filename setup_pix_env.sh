1c034680516978926c5ad32758f8aecfaae6a3cc
Pix <pix119@proton.me>
2023-12-02 00:47:33 +0800

    More to the config setup

--- setup_pix_env.sh
+++ setup_pix_env.sh
@@ -2,104 +2,196 @@
 
 set -e # Exit on error
 
-# Easy Colour printing!
-SUCCUSS="$(tput setaf 2)[S]:$(tput sgr0)"
-ERROR="$(tput setaf 1)[!]:$(tput sgr0)"
-INFO="$(tput setaf 4)[+]:$(tput sgr0)"
-WARN="$(tput setaf 3)[W]:$(tput sgr0)"
-INPUT="$(tput setaf 6)[>]:$(tput sgr0)"
 LOG="pix_install.log"
 
+echo "Running quick bootstrap..."
+# For some reason arch gum doesn't come with log -_-
+sudo pacman -Sy gum --noconfirm
+# May need to do this method..
+# sudo pacman -Sy git go --noconfirm
+# git clone https://github.com/charbracelet/gum.git
+# cd gum || return
+# go build
+# sudo cp ./gum /usr/bin
+# cd .. || return
+
+if ! command -v gum log; then
+	echo "Uncomment the extended bootstrap"
+	exit 1
+fi
+
+p() {
+	gum log -t kitchen -s -l none "$1"
+}
+
 info() {
-	printf "%s%s%s\n" "$INFO" "$1" "$NC"
+	gum log -t kitchen -s -l info "$1"
 }
 
 success() {
-	printf "%s%s%s\n" "$SUCCUSS" "$1" "$NC"
+	gum log -t kitchen -s -l info --prefix="SUCCESS" "$1"
 }
 
 warn() {
-	printf "%s%s%s\n" "$WARN" "$1" "$NC"
+	gum log -t kitchen -s -l warn "$1"
 }
 
 error() {
-	printf "%s%s\n" "$ERROR" "$1" "$NC" >&2
+	gum log -t kitchen -s -l error "$1"
 }
 
-printf "$INFO Running Pix's Arch Linux Hyprland and Development Environment Script\n"
+fatal() {
+	gum log -t kitchen -s -l fatal "$1"
+	exit 1
+}
+
+# # Easy Colour printing!
+# SUCCUSS="$(tput setaf 2)[S]:$(tput sgr0)"
+# ERROR="$(tput setaf 1)[!]:$(tput sgr0)"
+# INFO="$(tput setaf 4)[+]:$(tput sgr0)"
+# WARN="$(tput setaf 3)[W]:$(tput sgr0)"
+# INPUT="$(tput setaf 6)[>]:$(tput sgr0)"
+#
+# info() {
+# 	printf "%s%s%s\n" "$INFO" "$1" "$NC"
+# }
+#
+# success() {
+# 	printf "%s%s%s\n" "$SUCCUSS" "$1" "$NC"
+# }
+#
+# warn() {
+# 	printf "%s%s%s\n" "$WARN" "$1" "$NC"
+# }
+#
+# error() {
+# 	printf "%s%s%s\n" "$ERROR" "$1" "$NC" >&2
+# }
+
+info "Running Pix's Arch Linux Hyprland and Development Environment Script\n"
 sleep 2
-printf "$WARN Please remember to backup your files if this isn't a fresh install! \nThis script will pave over your config files\n"
+warn "Please remember to backup your files if this isn't a fresh install! " \
+	"\nThis script will pave over your config files\n"
 sleep 2
-printf "$WARN Some commands require you to enter a password to execute, you may need to baby sit this script.\n"
-printf "$WARN CTRL C or CTRL Q can quit this script if you need to!\n"
+warn "Some commands require you to enter a password to execute, you may need " \
+	"to baby sit this script.\n"
+warn "CTRL C or CTRL Q can quit this script if you need to!\n"
 sleep 2
 
 HAS_YAY=/sbin/yay
 
 if [ -f "$HAS_YAY" ]; then
-	printf "$INFO 'yay' was located, using it to install.\n"
+	info "'yay' was located, using it to install.\n"
 else
-	printf "$WARN Unable to locate 'yay' \n"
-	read -n1 -rep "$INPUT Would you like to install [Y/n]" INST
-	if [[ $INST =~ ^[Nn]$ ]]; then
-		printf "$ERROR An AUR helper is required for this script, feel free to update to your AUR Helper of choice. Now exiting\n"
+	warn "Unable to locate 'yay' \n"
+	p "Would you like to install?"
+	USER_IN=$(gum input --placeholder "[Y/n]")
+	if [[ $USER_IN =~ ^[Nn]$ ]]; then
+		error "An AUR helper is required for this script, feel free to update to " \
+			"your AUR Helper of choice. Now exiting\n"
 		exit 1
 	else
 		info "Installing YAY"
-		git clone https://aur.archlinux.org/yay.git
+		git clone https://aur.archlinux.org/yay.git | tree -a "$LOG"
 		cd yay
-		makepkg -si --noconfirm 2>&1 | tee -a $LOG
+		makepkg -si --noconfirm 2>&1 | tee -a "$LOG"
 		cd ..
 	fi
 	# Update System now.
-	printf "$INFO Updating system to avoid issues"
+	info "Updating system to avoid issues"
 	yay -Sy archlinux-keyring --noconfirm 2>&1 | tee -a $LOG
 	yay -Syu --noconfirm 2>&1 | tee -a $LOG
 fi
+USER_IN=""
 
 ### Install Packages ###
-read -n1 -rep "${INPUT} Would you like to install packages? [Y/n]" INST
-echo
-
+p "Would you like to install packages?"
+USER_IN=$(gum input --placeholder "[Y/n]")
 if [[ $INST =~ ^[Nn]$ ]]; then
 	warn "No packages installed. Ending Script"
 	exit 1
 else
-	## Use Midori Browser but download from the side direct, the AUR is a full major version behind.
-	GIT_PKGS="grimblast-git sddm-git hyprpicker-git waybar-hyprland-git"
-	HYPR_PKGS="hyprland wl-clipboard wf-recorder rofi wlogout swaylock-effects dunst swaybg"
-	FONT_PKGS="ttf-fira-code ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font otf-sora noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd adobe-source-code-pro-fonts"
-	APP_PKGS="discord wg-look-bin qt5ct btop gvfs ffmpegthumbs swww mousepad mpv playerctl pamixer noise-suppression-for-voice polkit-gnome ffmpeg viewnior pavucontrol thunar ffmpegthumbnailer tumbler thunar-archive-plugin xdg-user-dirs"
-	THEME_PKGS="nordic-theme papirus-icon-theme starship"
-	DEV_PKGS="eza jq sd fd neovim duf dust trippy wezterm alacritty lazygit zerotier-one ripgrep pixz rsync croc axel bat btop curl meson go-task fish fzf go glow gum htop less lua navi neofetch tealdeer tokei vim unzip wget zoxide jujutsu rm-improved go"
 
-	if ! yay -S --noconfirm $GIT_PKGS $HYPR_PKGS $FONT_PKGS $APP_PKGS $DEV_PKGS $THEME_PKGS 2>&1 | tee -a $LOG; then
+	## Use Midori Browser but download from the side direct, the AUR is a full major version behind.
+	#
+	#
+	# Need the Git version specifically.
+	# grimblast - Screenshot Utility
+	# SDDM - Simple Display Manager
+	# Hyprpicker - Pick colours from something!
+	# Waybar-Hyprland - Waybar but for Hyprland :>
+	GIT_PKGS="grimblast-git sddm-git hyprpicker-git waybar-hyprland-git"
+
+	# Specific to Hyprland Programs
+	# Hyprland - Window Manager.
+	# Hyprpaper - Wallpapers for Hyprland.
+	# wl-clipboard - Clipboard manager for Wayland.
+	# wf-recorder -
+	# Rofi - GUI Launcher program
+	# wlogout - Wayland logout.
+	# Swaylock-effects - Make swaylock look pretty <3
+	# dunst - Notification System :D
+	# Swappy - Screenshot / basic picture editor.
+	HYPR_PKGS="hyprland hyprpaper wl-clipboard wf-recorder rofi wlogout swaylock-effects dunst swappy"
+
+	FONT_PKGS="ttf-fira-code ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font otf-sora " \
+		"noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd adobe-source-code-pro-fonts"
+
+	# General APPS I like to use :>
+	# Discord - #TalkingToPeople
+	# btop/htop - Fancy looking top
+	APP_PKGS="discord wg-look-bin qt5ct btop gvfs ffmpegthumbs swww mousepad mpv playerctl pamixer " \
+		" noise-suppression-for-voice polkit-gnome ffmpeg viewnior pavucontrol thunar ffmpegthumbnailer" \
+		" tumbler thunar-archive-plugin xdg-user-dirs"
+	#
+	THEME_PKGS="nordic-theme papirus-icon-theme starship"
+
+	# Terminal Packages for Dev and general everything else.
+	# See the Toolset.MD for information.
+	TERM_PKGS="alacritty axel bat bear btop croc curl duf dust eza fastmod fd fish fzf glow go go-task gum htop jq" \
+		" jujutsu lazygit less lua meson navi neofetch neovim pixz procs ripgrep rm-improved rsync sd tealdeer" \
+		" tokei trippy unzip vim wezterm wget zerotier-one zoxide pacman-contrib"
+
+	if ! yay -S --noconfirm "$GIT_PKGS" "$HYPR_PKGS" "$FONT_PKGS" \
+		"$APP_PKGS" "$TERM_PKGS" "$THEME_PKGS" 2>&1 | tee -a "$LOG"; then
 		error "Failed to install additional packages - please check the install log\n"
 		exit 1
 	fi
 	xdg-user-dirs-update
 	success "All packages installed successfully!"
 fi
-
+USER_IN=""
 ### Config Files ###
 
-read -n1 -rep "$INPUT Would you like to copy config files? [Y/n]" CFG
-if [[ $CFG =~ ^[Nn]$ ]]; then
+p "Would you like to copy config files?"
+USER_IN=$(gum input --placeholder "[Y/n]")
+if [[ $USER_IN =~ ^[Nn]$ ]]; then
 	info "Config files not installed"
 else
 	info "Copying configuration files into .config"
-	# cp -r pixconfig/alacritty ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/dunst ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/fish ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/hypr ~/.config/ 2>&1 | tee -a $LOG
+	## Experiment with just doing a symlink?
+	# SYMLINK EXPERIMENT
+	ln -b -n pixconfig/dunst ~/.config/dunst
+	ln -b -n pixconfig/fish ~/.config/fish 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/hypr ~/.config/hypr 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/nvim ~/.config/nvim 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/pipewire ~/.config/pipewire 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/rofi ~/.config/rofi 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/swaylock ~/.config/swaylock 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/waybar ~/.config/waybar 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/wezterm ~/.config/wezterm 2>&1 | tee -a $LOG
+	ln -b -n pixconfig/wlogout ~/.config/wlogout 2>&1 | tee -a $LOG
+
+	# cp -r pixconfig/dunst ~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/fish ~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/hypr ~/.config/ 2>&1 | tee -a $LOG
 	# cp -r pixconfig/nvim ~/.config/ 2>&1 | tee -a $LOG
 	# cp -r pixconfig/pipewire ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/rofi ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/swaylock ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/waybar ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/waybar ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/wezterm ~/.config/ 2>&1 | tee -a $LOG
-	cp -r pixconfig/wlogout~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/rofi ~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/swaylock ~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/waybar ~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/wezterm ~/.config/ 2>&1 | tee -a $LOG
+	# cp -r pixconfig/wlogout ~/.config/ 2>&1 | tee -a $LOG
 
 	# Set some files as executable!
 	chmod +x ~/.config/hypr/xdg-portal-hyprland
@@ -108,16 +200,18 @@ fi
 
 ### Fonts for Waybar! ###
 mkdir -p ./waybar-setup/nerdfonts
-cd ./waybar-setup
+cd ./waybar-setup || return
 axel -n 4 https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCode.zip
 unzip '*.zip' -d ./nerdfonts
-rm -rf *.zip
+rm -rf ./*.zip
 sudo cp -R ./nerdfonts/ /usr/share/fonts/
 
 fc-cache -rv
 
-read -n1 -rep 'Would you like to enable SDDM autologin [Y/n]' SDDM
-if [[ $SDDM =~ ^[nN]$ ]]; then
+USER_IN=""
+p "Would you like to enable SDDM autologin?"
+USER_IN=$(gum input --placeholder "[Y/n]")
+if [[ $USER_IN =~ ^[nN]$ ]]; then
 	info "Not enabling auto login!"
 else
 	LOC="/etc/sddm.conf"
@@ -127,13 +221,15 @@ else
 	sudo systemctl enable sddm
 	sleep 3
 fi
+USER_IN=""
 
 ### Bluetooth ###
-read -n1 -rep "$INPUT Optional! - Would you like to install bluetooth packages? [y/N]" BT
-if [[ $BT =~ ^[yY]$ ]]; then
+p "Optional! - Would you like to install bluetooth packages?"
+USER_IN=$(gum input --placeholder "[y/N]")
+if [[ $USER_IN =~ ^[yY]$ ]]; then
 	info "Instaling Bluetooth Packages!..."
 	BLUE_PKG="bluez bluez-utils blueman"
-	if ! yay -S --noconfirm $BLUE_PKG 2>&1 | tee -a $LOG; then
+	if ! yay -S --noconfirm "$BLUE_PKG" 2>&1 | tee -a "$LOG"; then
 		error "Failed to install bluetooth packages - please check the install log."
 	else
 		sudo systemctl enable --now bluetooth.service
@@ -144,8 +240,10 @@ else
 fi
 
 success "Installation Completed!"
-read -n1 -rep "$INPUT Would you like to start Hyprland now? [Y/n]" HYPR
-if [[ $HYPR =~ ^[Nn]$ ]]; then
+USER_IN=""
+p "Would you like to start Hyprland now?"
+USER_IN=$(gum input --placeholder "[Y/n]")
+if [[ $USER_IN =~ ^[Nn]$ ]]; then
 	info "Finishing..."
 	exit 0
 else
