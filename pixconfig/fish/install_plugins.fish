4e1fd5f7524d6791a6381276f75c8bac4891daa5
Pix <ben.dennis@hyprfire.com>
2024-01-09 11:56:51 +0800

    The install function.

--- pixconfig/fish/install_plugins.fish
+++ pixconfig/fish/install_plugins.fish
@@ -6,3 +6,7 @@ for f in (cat ./bak.fish_plugins)
     echo "Installing $f"
     fisher install $f
 end
+
+echo "Configuring Tide!"
+
+tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Sharp --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, frame' --prompt_connection=Solid --powerline_right_prompt_frame=Yes --prompt_spacing=Compact --icons='Many icons' --transient=No
