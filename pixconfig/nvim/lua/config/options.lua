87edc43d24939c34a7aa048524baba43cc80310c
Pix <pix119@proton.me>
2024-01-14 22:38:04 +0800

    Adding some options

--- pixconfig/nvim/lua/config/options.lua
+++ pixconfig/nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 10 -- Number of lines to keep above and below the cursor
+vim.opt.signcolumn = "yes"
+
+vim.opt.colorcolumn = "100"
