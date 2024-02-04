8ef1d32b4ebbf3a2b8faafd15fea19ffae12b8b4
Pix-xiP <pix119@proton.me>
2024-02-05 00:38:52 +0800

    Oop

--- pixconfig/nvim/lua/plugins/cmp.lua
+++ pixconfig/nvim/lua/plugins/cmp.lua
@@ -62,7 +62,7 @@ return {
           cmp.config.compare.exact,
           cmp.config.compare.sort_text,
           cmp.config.compare.score,
-          require("cmp-under-comparator").under,
+          -- require("cmp-under-comparator").under,
           cmp.config.compare.recently_used,
           cmp.config.compare.kind,
           cmp.config.compare.length,
