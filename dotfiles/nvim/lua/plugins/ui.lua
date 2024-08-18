75b0bc224355aa1079116481e19c4b99184c4369
Pix xiP <pix119@proton.me>
2024-08-18 20:22:36 +0800

    Adding markdown viewer

--- dotfiles/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/lua/plugins/ui.lua
@@ -322,6 +322,14 @@ return {
       },
     },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
   -- {
   --   "tris203/precognition.nvim",
   --   --event = "VeryLazy",
