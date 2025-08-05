5bdd0234194557dd23130017d0508a1cfabefc52
Pix xiP <pix119@proton.me>
2025-08-05 14:37:26 +0800

    experimenting

--- dotfiles/nvim/lua/plugins/dev.lua
+++ dotfiles/nvim/lua/plugins/dev.lua
@@ -37,7 +37,13 @@ return {
 	{
 		"supermaven-inc/supermaven-nvim",
 		config = function()
-			require("supermaven-nvim").setup({})
+			require("supermaven-nvim").setup({
+				-- keymaps = {
+				-- 	accept_suggestion = "<S-Tab>",
+				-- 	clear_suggestion = "<C-]>",
+				-- },
+			})
+
 			-- 	keymaps = {
 			-- 		accept_suggestion = "<Tab>",
 			-- 		clear_suggestion = "<C-]>",
