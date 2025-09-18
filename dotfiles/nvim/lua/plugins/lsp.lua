b0e029db824335a17bd98df111d846f42e1d66d8
Pix xiP <pix119@proton.me>
2025-09-18 15:12:59 +0800

    update: taplo

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -167,9 +167,7 @@ return {
 				taplo = {
 					cmd = { "taplo", "lsp", "stdio" },
 					filetypes = { "toml" },
-					root_dir = function(fname)
-						return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
-					end,
+					root_dir = require("lspconfig.util").root_pattern("*.toml", ".git"),
 				},
 
 				-- -- Odin Language Server
