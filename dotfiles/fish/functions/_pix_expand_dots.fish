23c5d800133d96df8e9cb40736ba57335088b4f0
Pix xiP <pix119@proton.me>
2025-11-21 14:31:01 +0800

    auto expander for fish

---
+++ dotfiles/fish/functions/_pix_expand_dots.fish
@@ -0,0 +1,9 @@
+function _pix_expand_dots
+    if commandline --search-field >/dev/null
+        commandline --search-field --insert '.'
+    else if string match --quiet --regex -- '^(\.\./)*\.\.$' "$(commandline --current-token)"
+        commandline --insert '/..'
+    else
+        commandline --insert '.'
+    end
+end
