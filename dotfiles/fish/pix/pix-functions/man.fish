51cf6bbc2c1825c2eaa373c6c76b2ca4ffaefafa
Pix xiP <pix119@proton.me>
2026-01-10 22:20:36 +0800

    more descriptive of what its acutally doing

--- dotfiles/fish/pix/pix-functions/man.fish
+++ dotfiles/fish/pix/pix-functions/man.fish
@@ -3,15 +3,15 @@ function man --wraps man --description "Format and display man pages"
     set -l ubmral_blue (set_color -o 6c4dff)
     set -l umbral_yellow (set_color ffd369)
     set -l umbral_orange (set_color -u e9729d)
-    set -l end (printf "\e[0m")
+    set -l reset_normal (printf "\e[0m")
 
     set -lx LESS_TERMCAP_mb $umbral_red # _mb = BLINK
     set -lx LESS_TERMCAP_md $ubmral_blue # _md = BOLD
-    set -lx LESS_TERMCAP_me $end # _me = END SPECIAL MODE
+    set -lx LESS_TERMCAP_me $reset_normal # _me = END SPECIAL MODE
     set -lx LESS_TERMCAP_so $umbral_yellow # _so = STANDOUT
-    set -lx LESS_TERMCAP_se $end # _se = END SPECIAL MODE
+    set -lx LESS_TERMCAP_se $reset_normal # _se = END SPECIAL MODE
     set -lx LESS_TERMCAP_us $umbral_orange # _us = UNDERLINE
-    set -lx LESS_TERMCAP_ue $end # _ue = REMVOVE UNDERLINE
+    set -lx LESS_TERMCAP_ue $reset_normal # _ue = REMVOVE UNDERLINE
     set -lx LESS '-R -s'
     set -lx GROFF_NO_SGR yes
 
