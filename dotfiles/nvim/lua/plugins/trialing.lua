18d3f71fdff8642c5d9e492cce3f6c4b3226576b
Pix xiP <pix119@proton.me>
2026-04-13 10:13:41 +0800

    hmm..

--- dotfiles/nvim/lua/plugins/trialing.lua
+++ dotfiles/nvim/lua/plugins/trialing.lua
@@ -2,4 +2,13 @@
 return {
 	-- Fennel in Neovim
 	{ "Olical/nfnl", ft = "fennel" },
+	{
+		"martindur/zdiff.nvim",
+		cmd = "Zdiff",
+		keys = {
+			{ "<leader>zd", "<cmd>Zdiff<cr>", desc = "Zdiff (uncommitted)" },
+			{ "<leader>zD", "<cmd>Zdiff main<cr>", desc = "Zdiff (vs main)" },
+		},
+		opts = {},
+	},
 }
