80382b081cec7388c4cf900dc84870c38f8da632
Pix xiP <pix119@proton.me>
2026-05-31 02:08:34 +0800

    illogial impulse base layer for hyprland config

---
+++ dotfiles/hypr/custom/rules.lua
@@ -0,0 +1,3 @@
+-- This file will not be overwritten across dots-hyprland updates.
+-- The file name is for the sake of organization and does not matter
+-- See the corresponding files in ~/.config/hypr/hyprland for examples
