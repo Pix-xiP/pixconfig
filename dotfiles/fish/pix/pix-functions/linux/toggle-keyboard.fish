e5565a2100421a4130dc17e6ebdbf2633b0bdde5
Pix xiP <pix119@proton.me>
2026-05-31 02:03:27 +0800

    improved keyboard swap function

--- dotfiles/fish/pix/pix-functions/linux/toggle-keyboard.fish
+++ dotfiles/fish/pix/pix-functions/linux/toggle-keyboard.fish
@@ -1,26 +1,11 @@
-function toggle-keyboard --description "Toggles keyboard between colemak && qwerty"
-    set -l input_dir $HOME/.config/hypr/pix.hypr
-    set -l target $input_dir/input.conf
-    set -l colemak $input_dir/colemak.input.conf
-    set -l qwerty $input_dir/qwerty.input.conf
+function toggle-keyboard --description "Toggle keyboard between colemak && qwerty"
+    set -l variant (hyprctl getoption input:kb_variant -j | jq -r '.str')
 
-    # Use 'readlink' and 'string match' for cleaner logic
-    if test -L $target
-        set -l current (readlink $target)
-
-        # String match is faster and more readable than basename + test
-        if string match -q "*colemak*" $current
-            echo "Switching to QWERTY..."
-            ln -sf $qwerty $target
-        else
-            echo "Switching to Colemak..."
-            ln -sf $colemak $target
-        end
+    if test "$variant" = colemak
+        hyprctl eval 'hl.config({ input = { kb_layout = "us", kb_variant = "" }})'
+        notify-send Keyboard "Switching to Qwerty"
     else
-        echo "Initializing Colemak layout..."
-        ln -sf $colemak $target
+        hyprctl eval 'hl.config({ input = { kb_layout = "us", kb_variant = "colemak" }})'
+        notify-send Keyboard "Switching to Colemak"
     end
-
-    echo "Current layout: "(readlink $target | path basename)
-    hyprctl reload
 end
