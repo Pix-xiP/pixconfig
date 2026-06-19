4eddbc8f980863da7e3f2d6551ed404ec052ecb5
Pix xiP <pix119@proton.me>
2026-06-20 02:20:52 +0800

    add some hyprland.starts

--- dotfiles/hypr/pix/execs.lua
+++ dotfiles/hypr/pix/execs.lua
@@ -1,6 +1,8 @@
 hl.on("hyprland.start", function()
-	-- hl.exec_cmd("ghostty", { workspace = 1, silent = true })
+	hl.exec_cmd("ghostty", { workspace = "5 silent" })
 	-- hl.exec_cmd("zen-browser", { workspace = 7, silent = true })
 	-- hl.exec_cmd("discord", { workspace = 8, silent = true })
 	-- hl.exec_cmd("signal-desktop", { workspace = 9, silent = true })
+
+	hl.exec_cmd("hyprctl setcursor GoogleDot-Black 24")
 end)
