c9220da288080aada5f3c0bb8e4d979a889497f2
Pix xiP <pix119@proton.me>
2026-06-21 00:30:53 +0800

    update go-tools

--- setup_scripts/go-tools.sh
+++ setup_scripts/go-tools.sh
@@ -10,13 +10,12 @@ go install golang.org/x/tools/cmd/deadcode@latest                     # find dea
 go install golang.org/x/tools/cmd/godoc@latest                        # Documentation
 go install golang.org/x/tools/gopls@latest                            # language server
 go install gotest.tools/gotestsum@latest                              # Testing
-go install github.com/cweill/gotests/...
+go install github.com/cweill/gotests/gotests@latest
 go install mvdan.cc/gofumpt@latest # stricter code formatting
 
 # General tools
 go install github.com/Owloops/updo@latest         # check if something is online!
 go install github.com/charmbracelet/freeze@latest # Screenshot your code :>
-go install github.com/dhth/omm@latest             # On my mind todo app
 go install github.com/dim-an/cod@latest           # Code completion on anything
 go install github.com/TypicalAM/goread@latest     # RSS Reader
 
