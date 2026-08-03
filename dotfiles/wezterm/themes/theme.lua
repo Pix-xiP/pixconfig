43238ff9e7e0a26a9d6648f1a57e6af5f70618d0
Pix xiP <pix119@proton.me>
2026-08-03 12:05:48 +0800

    testing wez

--- dotfiles/wezterm/themes/theme.lua
+++ dotfiles/wezterm/themes/theme.lua
@@ -13,8 +13,8 @@ T.themes = theme_list
 T.default = "Rosé Pine Moon (base16)"
 
 if utils.os == "Linux" then
-	-- T.font = wez.font("Maple Mono", { weight = "Regular", italic = false})
-	T.font = wez.font("FiraCode", { weight = "Bold", italic = false })
+	T.font = wez.font("Maple Mono", { weight = "Regular", italic = false })
+	-- T.font = wez.font("FiraCode", { weight = "Bold", italic = false })
 	T.font_size = 11.0
 elseif utils.os == "Darwin" then
 	-- T.font = wez.font("FiraCode Nerd Font", { weight = "Regular", italic = false })
