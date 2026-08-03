6042cf0934c0e9ccc40fe6deaeb6095fa430a7b2
Pix xiP <pix119@proton.me>
2026-08-03 21:56:03 +0800

    execs

--- dotfiles/hypr/pix/execs.lua
+++ dotfiles/hypr/pix/execs.lua
@@ -1,8 +1,11 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("ghostty", { workspace = "5 silent" })
-	-- hl.exec_cmd("zen-browser", { workspace = 7, silent = true })
+	hl.exec_cmd("zen-browser", { workspace = "7 silent" })
 	-- hl.exec_cmd("discord", { workspace = 8, silent = true })
 	-- hl.exec_cmd("signal-desktop", { workspace = 9, silent = true })
+
+	-- Steam Stuff
+	hl.exec_cmd("xrandr --output DP-1 --primary")
 
 	hl.exec_cmd("hyprctl setcursor GoogleDot-Black 24")
 end)
