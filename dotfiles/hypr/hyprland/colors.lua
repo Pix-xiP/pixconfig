238790af31db6ec5df47ee27a6d56f06bbff6d9a
Pix xiP <pix119@proton.me>
2026-08-17 01:28:50 +0800

    oop

--- dotfiles/hypr/hyprland/colors.lua
+++ dotfiles/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(46464f77)",
-            inactive_border = "rgba(1b1b2133)",
+            active_border   = "rgba(44464f77)",
+            inactive_border = "rgba(1a1b2033)",
         },
     },
     misc = {
-        background_color = "rgba(131318FF)",
+        background_color = "rgba(121318FF)",
     },
 })
 
-hl.window_rule({
+hl.window_rule({ -- not sure how to syntax "pin 1"
     match        = { pin = 1 },
-    border_color = "rgba(bbc3ffAA) rgba(bbc3ff77)",
+    border_color = "rgba(afc6ffAA) rgba(afc6ff77)",
 })
