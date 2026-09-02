629406991b7c40db9a71666824a5b38bb627c942
Pix xiP <pix119@proton.me>
2026-09-03 03:07:06 +0800

    updates.

--- dotfiles/hypr/custom/keybinds.lua
+++ dotfiles/hypr/custom/keybinds.lua
@@ -1,3 +1,1 @@
--- This file will not be overwritten across dots-hyprland updates.
--- The file name is for the sake of organization and does not matter
--- See the corresponding files in ~/.config/hypr/hyprland for examples
+hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
--- dotfiles/hypr/custom/variables.lua
+++ dotfiles/hypr/custom/variables.lua
@@ -1,3 +1,1 @@
--- This file will not be overwritten across dots-hyprland updates.
--- The file name is for the sake of organization and does not matter
--- See the corresponding files in ~/.config/hypr/hyprland for examples
+
